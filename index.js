8850de33f4404dd2fb03d51dd68e3f9462de019e
CatherineHonors <cathonors@gmail.com>
2022-04-05 16:51:27 -0400

    commit tree 3f4cf9d9eff7ad224703aa37c1a50d27254d3bc0

---
+++ index.js
@@ -0,0 +1,15 @@
+// const add = (parameter1, parameter2) => parameter1 + parameter2;
+// add(2,3);
+
+
+const divide = () => {
+    return (2000/100);
+}
+
+const square = (parameter1) => {
+    return (parameter1*parameter1);
+}
+
+const add = (parameter2, parameter3) => {
+    return (parameter2 + parameter3);
+}
